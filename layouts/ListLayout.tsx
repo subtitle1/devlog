961502ae28bfa0c92c97e9a9c098928d17a65f51
subtitle1 <subtitle1@naver.com>
2023-07-03 19:47:51 +0900

    about 수정

--- layouts/ListLayout.tsx
+++ layouts/ListLayout.tsx
@@ -81,7 +81,7 @@ export default function ListLayout({
           <h1 className="text-lg font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-4xl md:leading-14">
             {title}
           </h1>
-          <div className="relative max-w-lg">
+          <div className="relative">
             <label>
               <span className="sr-only">Search articles</span>
               <input
--- pages/index.tsx
+++ pages/index.tsx
@@ -26,10 +26,10 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1 className="font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
-            안녕하세요! 🖐️
+            여기에 뭐 넣지
           </h1>
           <p className="text-base leading-7 text-gray-500 dark:text-gray-400">
-            개발 및 취미로 하는 블로그입니다
+            룰루랄라 
           </p>
         </div>
         <div className="mx-auto my-12 grid max-w-xl grid-cols-1 justify-between gap-y-20 lg:max-w-none lg:grid-cols-2">

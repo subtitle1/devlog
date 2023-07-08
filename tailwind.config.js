43b83632811a273e6f2b28cb5dc24effb7edae1a
subtitle1 <subtitle1@naver.com>
2023-07-08 22:42:40 +0900

    edit config

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,7 +25,7 @@ module.exports = {
       fontSize: {
         fontSize: {
           sm: '0.8rem',
-          base: '1.6rem',
+          base: '1rem',
           xl: '1.25rem',
           '2xl': '1.563rem',
           '3xl': '1.953rem',

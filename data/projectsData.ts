f4c5ea15f12403bc63dd36c360b0bcf728d0d6fd
subtitle1 <subtitle1@naver.com>
2023-07-09 12:48:44 +0900

    edit projects Data

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -3,14 +3,14 @@ const projectsData = [
     title: 'HTA-CINEMA',
     description: `MEGABOX Clone으로, Java/SpringBoot 및 Bootstrap으로 홈페이지 구현. 영화 데이터는 TMDB API로 가져왔다.`,
     imgSrc: '/static/images/projects-thumb/HTA-CINEMA.jpg',
-    tags: ['HTML5', 'CSS3', 'JQuery', 'JavaScript', 'Java', 'SpringBoot'],
+    tags: ['HTML5', 'CSS3', 'JQUERY', 'JAVASCRIPT', 'JAVA', 'SPRINGBOOT'],
     href: 'https://github.com/subtitle1/hta-cinema',
   },
   {
     title: 'Git blog',
     description: `첫번째 개인용 블로그`,
     imgSrc: '/static/images/projects-thumb/git-blog.png',
-    tags: ['HTML', 'CSS', 'JAVSCRTIPC', 'RUBY'],
+    tags: ['HTML', 'CSS', 'JAVASCRIPT', 'RUBY'],
     href: 'https://github.com/subtitle1/subtitle1.github.io',
   },
 ]

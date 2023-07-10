7b554c638dea3f9745cc1acf56a58353bb72961c
subtitle1 <subtitle1@naver.com>
2023-07-10 18:50:56 +0900

    edit congif

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -8,8 +8,8 @@ const siteMetadata = {
   description: 'Stella의 devlog ',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://devtella.vercel.app',
+  siteRepo: 'https://github.com/subtitle1/devlog',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',

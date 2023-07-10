f3252b7cd5f64f81fbeb9d3a408097c6893da607
subtitle1 <subtitle1@naver.com>
2023-07-10 19:10:11 +0900

    edit config

--- components/SectionContainer.tsx
+++ components/SectionContainer.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export default function SectionContainer({ children }: Props) {
   return (
-    <section className="mx-auto max-w-3xl border-slate-100 px-6 bg-white dark:border-gray-800 dark:bg-[#18181B] sm:px-6 xl:max-w-4xl xl:px-14">
+    <section className="mx-auto max-w-3xl border-slate-100 bg-white dark:border-gray-800 dark:bg-[#18181B] px-3 sm:px-6 xl:max-w-4xl xl:px-14">
       {children}
     </section>
   )

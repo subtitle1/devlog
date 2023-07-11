88995f8ff793589491d73133fc569a6b2ba6281c
subtitle1 <subtitle1@naver.com>
2023-07-11 16:20:24 +0900

    change fonts / fix mobile

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -38,44 +38,47 @@ const MobileNav = () => {
         </svg>
       </button>
       <div
-        className={`fixed left-0 top-0 z-10 h-full w-full transform opacity-95 duration-300 ease-in-out ${
-          navShow ? 'translate-x-0' : 'translate-x-full'
+        className={`fixed left-0 top-0 z-100 h-full w-full transform opacity-95 duration-300 ease-in-out ${
+          navShow ? 'translate-x-0' : 'translate-x-full opacity-0'
         }`}
       >
         <nav className="mt-5 bg-white dark:bg-[#18181B] rounded-2xl">
-          <div className='flex justify-between'>
-            {headerNavLinks.map((link) => (
-              <div key={link.title} className="px-12 py-4">
-                <Link
-                  href={link.href}
-                  className="text-2xl font-bold tracking-widest text-gray-900 dark:text-gray-100"
-                  onClick={onToggleNav}
-                >
-                  {link.title}
-                </Link>
-              </div>
-            ))}
-              <div className="justify-end p-3">
-                <button
-                  className=" h-5 w-5 rounded"
-                  aria-label="Toggle Menu"
-                  onClick={onToggleNav}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    className="text-gray-900 dark:text-gray-100"
+          <div className='absolute top-0 left-0 w-screen h-screen bg-slate-100/70'>
+            <div className='flex flex-col justify-center items-center h-full w-full z-200 '>
+              {headerNavLinks.map((link) => (
+                <div key={link.title} className="px-12 py-4">
+                  <Link
+                    href={link.href}
+                    className="box-border text-pink-500 text-3xl font-bold tracking-widest"
+                    onClick={onToggleNav}
                   >
-                    <path
-                      fillRule="evenodd"
-                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </button>
+                    {link.title}
+                  </Link>
+                </div>
+              ))}
+                <div className="justify-end p-3">
+                  <button
+                    className=" h-5 w-5 rounded"
+                    aria-label="Toggle Menu"
+                    onClick={onToggleNav}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      className="text-gray-900 dark:text-gray-100"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </button>
+                </div>
               </div>
-            </div>
+            
+          </div>
           </nav>
         </div>
       </div>
